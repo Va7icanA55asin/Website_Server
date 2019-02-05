9e9dc8e8418cf9243cb35219a6ac4751a487ed3d
GCblandin <31488362+GCblandin@users.noreply.github.com>
2019-02-04 21:47:22 -0600

    Initial commit

---
+++ src/main/java/com/github/va7icana55asin/website_server/WebsiteServerApplication.java
@@ -0,0 +1,14 @@
+package com.github.va7icana55asin.website_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsiteServerApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/github/va7icana55asin/website_server/WebsiteServerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.github.va7icana55asin.website_server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebsiteServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
